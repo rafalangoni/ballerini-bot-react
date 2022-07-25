9262430479be7db4f3b9e02c92ed969d072a92e8
Rafael Langoni <langoninaves@gmail.com>
2022-07-25 19:17:03 -0300

    projeto finalizado

--- src/componentes/body/index.js
+++ src/componentes/body/index.js
@@ -2,14 +2,23 @@ import './Body.css'
 
 const Body = () => {
     return (
-        <section className='secao__principal'>
-            <div>
-                <h1>Balle Bot</h1>
-                <h3>Deixe a sua comunidade segura de um jeito fácil</h3>
-                <button>Me adicione!</button>
-            </div>
-            <img src='./imagens/imagem-principal.png' alt='imagem principal do site, um robô'></img>
-        </section>
+        <>
+            <section className='secao__principal'>
+                <div>
+                    <h1>Balle Bot</h1>
+                    <h3>Deixe a sua comunidade segura de um jeito fácil</h3>
+                    <button className='botao'>Me adicione!</button>
+                </div>
+                <img src='./imagens/imagem-principal.png' alt='imagem principal do site, um robô'></img>
+            </section>
+
+            <section className='secao__texto'>
+                <h2>O que ela faz por você?</h2>
+                <p>1. Cria um sistema de verificação capcha para novos membros garantirem que não são robôs</p>
+                <p>2. Possui um sistema de banimento automático para links suspeitos que forem enviados na comunidade</p>
+                <p>3. Sistema de avisos e banimento manual, para pessoas administradoras utilizarem</p>
+            </section>
+        </>
     )
 }
 
--- src/componentes/header/index.js
+++ src/componentes/header/index.js
@@ -1,15 +1,17 @@
 import './Header.css'
 
 const Header = () => {
-    return(
+    return (
         <header className='cabecalho'>
-            <img src='/imagens/logo.png' alt='logotipo do site ballerini bot'/>
-            <ul className='cabecalho__menu'>
-                <li>Comunidade Balerini</li>
-                <li>Tutorial</li>
-                <li>Open Source</li>
-                <li>Comandos</li>
-            </ul>
+            <img src='/imagens/logo.png' alt='logotipo do site ballerini bot' />
+            <nav>
+                <ul className='cabecalho__menu'>
+                    <li className='cabecalho__link'>Comunidade Balerini</li>
+                    <li className='cabecalho__link'>Tutorial</li>
+                    <li className='cabecalho__link'>Open Source</li>
+                    <li className='cabecalho__link'>Comandos</li>
+                </ul>
+            </nav>
         </header>
     )
 }
